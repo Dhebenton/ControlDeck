808b4ddd98e835659f3e94f6d2a3e6e41afff126
Daniil Hebenton <daniil.hebenton@gmail.com>
2025-06-23 21:57:21 +0100

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  base: '/', // Required for root-relative paths on Vercel
   build: {
-    outDir: 'dist'
+    outDir: 'dist', // Vercel auto-detects this
   },
   server: {
-    historyApiFallback: true
+    fs: {
+      strict: true
+    }
   }
-})
+});
